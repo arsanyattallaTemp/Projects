5a0d7569f3bad971e2eea90a618e88be610b8508
arsanyattallaTemp <101680208+arsanyattallaTemp@users.noreply.github.com>
2022-12-23 11:15:03 -0800

    Update Testing.java

--- src/test/test/java/test/java/Testing.java
+++ src/test/test/java/test/java/Testing.java
@@ -38,6 +38,7 @@ public class Testing {
         desiredCapabilities.setCapability("appium:deviceName", "Nexus6");
         desiredCapabilities.setCapability("appium:ensureWebviewsHavePages", true);
         desiredCapabilities.setCapability("autoGrantPermissions", true);
+        desiredCapabilities.setCapability("appWaitForLaunch", "true");
         UiAutomator2Options options = new UiAutomator2Options();
         options.setNativeWebScreenshot(true);
 
